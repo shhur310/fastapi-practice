c86f7ea23192473595d33f302216e5b390c417f8
shhur310 <seunghyehur@gmail.com>
2026-04-08 01:19:06 +0900

    first commit

---
+++ request.py
@@ -0,0 +1,7 @@
+# 요청 본문의 데이터 형식 관리
+from pydantic import BaseModel, Field
+
+# 사용자 추가할 때, 클라이언트가 서버로 보내는 데이터의 형식
+class UserCreateRequest(BaseModel):
+    name: str = Field(..., min_length=2, max_length=10)
+    job: str
